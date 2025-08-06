447852e14daf9c3adb938046c4497d6ec5c43196
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-06 13:46:31 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
-import Section from './components/Section'
 
+import './App.css'
+import Button from './components/Button'
+import Input from './components/Input'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <Section />
-        <h1>hello React!</h1>
-      </div>
-    </>
+    <div>
+      <Input
+        inputValue={'hello react'}
+        title={'input title'}
+        placeholder={'입력하세요'} />
+      <h1>hello React!</h1>
+      <Button text={'메일'} color={'red'} />
+      <Button text={'카페'} color={'blue'} />
+      <Button text={'블로그'} color={'orange'} />
+      <Button text={'기본값 설정'} />
+      <Button text={'자식요소 설정'} color={'pink'}>
+        <strong>중요</strong>
+      </Button>
+    </div>
   )
 }
 
