ea685c7fc9541e3fd23df40a3db967ae975d5a78
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-13 15:53:48 +0900

    uqdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,72 @@
 
-import './App.scss'
-import { useState, useRef, useEffect } from 'react'
-import { searchWikiTitles } from './api/wiki'
-
+import './App.css'
+import WeatherCard from './components/WeatherCard'
+import { useState,useRef,useEffect } from 'react'
+import { fetchCoordinates } from './api/geo'
+import { fetchWeatherByCoords } from './api/weather'
 function App() {
-  const [q, setQ] = useState('')
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [err, setErr] = useState('')
-  const inputRef = useRef(null)
 
-  useEffect(() => {
+  const [city, setCity]=useState('seoul')
+  const [weather, setWeather]=useState(null)
+  const [loading, setLoading]=useState(false)
+  const [err, setErr]=useState('')
+  const inputRef =useRef(null)
+
+  useEffect(()=>{
     inputRef.current.focus()
-  
+  },[])
 
-  }, [])
 
-  const handleSearch = async () => {
-    const keyword = q.trim()
+  const handleSearch=async()=>{
+    const q = city.trim()
+    if(!q) return
 
-    if (!keyword) return
 
     try {
       setLoading(true)
       setErr('')
+      
+      const {lat, lon, name, country}=await fetchCoordinates(q)
+      // console.log(lat, lon, name, country)
 
-      const pages = await searchWikiTitles(keyword)
-      setItems(pages)
-      setQ('')
+      const data = await fetchWeatherByCoords(lat, lon)
+      setWeather(data)
+      setCity('')
+      
     } catch (error) {
       console.log(error)
-      setErr("검색중 오류 발생")
-      setItems([])
-    } finally {
+      
+    }finally{
+      
       setLoading(false)
-      inputRef.current?.focus()
     }
 
   }
 
+
+  const onChangeInput=(e)=>setCity(e.target.value)
+  const onKeyup=(e)=>{
+    if(e.key==='Enter') handleSearch()
+  }
+
   return (
     <div className='app'>
-      <h1>위키백과 검색</h1>
-     
-      <div className="search">
-        <input
-          ref={inputRef}
-          value={q}
-          onChange={(e) => setQ(e.target.value)}
-          onKeyUp={(e) => e.key === 'Enter' && handleSearch()}
-          type="text" placeholder='검색어를 입력하세요(예:서울, 한글, 리액트)' />
+      <h1>최선호의 날씨앱</h1>
+      <div className="input-wrap">
+        <input 
+        ref={inputRef}
+        value={city}
+        onChange={onChangeInput}
+        onKeyUp={onKeyup}
+        type="text" 
+        placeholder='도시이름을 입력하세요' />
         <button onClick={handleSearch} disabled={loading}>
-          {loading? "검색 중....":"검색"}
+          {loading? "검색중....":"검색"}
           </button>
       </div>
-      {err &&  <div className="error">에러 코드</div>}
-      <ul className='list'>
-        {items.map((p) => (
-
-          <li key={p.id}>
-            <div>
-              <strong>{p.title}</strong>
-              <p>{p.description || '요약없음'}</p>
-              <a href={`https://ko.wikipedia.org/wiki/${encodeURIComponent(p.title)}`} target='_blank'>위키에서 보기</a>
-            </div>
-          </li>
-        ))}
-
-      </ul>
+      {err && <p className='error'>{err}</p>}
+      {loading && <p className='info'>불러오는중...</p>}
+      <WeatherCard weather={weather}/>
     </div>
   )
 }
