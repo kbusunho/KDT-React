f571afa46b3f8cac42d31a8a19affddb75cc8f92
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-18 11:37:14 +0900

    update

---
+++ frontend/component/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <h4>Todo List 🌱</h4>
+      <input placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
