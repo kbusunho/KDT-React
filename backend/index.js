091d86ead559e4f1859dcbdb63f0396f0d0abde5
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-19 11:07:25 +0900

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const cors = require("cors")
+const mongoose=require("mongoose")
+const dotenv =require("dotenv")
+const cors=require("cors")
 
 dotenv.config()
 
@@ -9,24 +9,27 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB 연결 성공"))
-  .catch((err) => console.log("연결 실패", err))
-
-const todoRoutes = require('./routes/todoRoutes')
-app.use('/api/todos', todoRoutes)
-
 app.use(cors({
-  origin: process.env.FRONT_ORIGIN,
-  credentials: true
+    origin:process.env.FRONT_ORIGIN,
+    credentials:true
 }))
 
-app.get('/', (req, res) => {
-  res.send("Hello Express")
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>console.log("MongoDB 연결 성공"))
+    .catch((err)=>console.log("연결 실패",err))
+
+
+
+const todoRoutes = require('./routes/todoRoutes')
+app.use('/api/todos',todoRoutes)
+
+
+
+app.get('/',(req, res)=>{
+    res.send("Hello Express")
 })
 
-app.listen(PORT, () => {
-  console.log("Server is Running!")
+app.listen(PORT,()=>{
+    console.log("Server is Running!")
 })
